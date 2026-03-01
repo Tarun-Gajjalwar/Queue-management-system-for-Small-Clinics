659c1ea7eb7525f8f043716495422569d55a336d
Tarun Gajjalwar <tarungajjalwar555@gmail.com>
2026-03-01 13:05:40 +0530

    added html,css,js files in backend

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,6 +6,7 @@ const clinicRoutes = require("./routes/clinic.routes");
 const Clinic_Card = require("./routes/clinicCard.routes");
 const patient_Appointment = require("./routes/patient.routes");
 const cors = require("cors");
+const path  = require("path")
 
 
 const app = express();
@@ -15,11 +16,17 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(cookieParser());
+app.use(express.static("./public"));
 
 app.use("/api/auth",authRouter);
 app.use("/api/auth/clinic", clinicRoutes);
 app.use("/api/clinic/card",Clinic_Card);
 app.use("/api/user/appointment",patient_Appointment);
+
+
+app.use("*name",(req,res)=>{
+    res.sendFile(path.join(__dirname,"../public/index.html"))
+})
 
 
 module.exports = app;
